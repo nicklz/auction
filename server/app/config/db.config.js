5787d22a3538b07bd283f63fc14c6ff6e4bbfc1c
Nick Kuhn <nkuhn@tesla.com>
2022-04-28 14:04:44 -0700

    revert db config

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -12,19 +12,3 @@ module.exports = {
     idle: 10000
   }
 };
-
-
-module.exports = {
-  HOST: '0.0.0.0',
-  USER: 'root',
-  PASSWORD: 'project',
-  DB: 'project',
-  port: 3307,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
